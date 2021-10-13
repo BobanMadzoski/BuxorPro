547212dda40eaa066da69d014648bf95947ecaf0
BobanMadzoski <44544610+BobanMadzoski@users.noreply.github.com>
2021-10-13 10:16:36 +0200

    update

--- js/gsap.js
+++ js/gsap.js
@@ -1,3 +1,4 @@
 gsap.registerPlugin(ScrollTrigger);
 
-ScrollTrigger.batch("div", {onEnter: batch => gsap.fromTo(batch, { duration:1 , opacity: 0, stagger: 0.1, y:100 }, {duration: 1, opacity: 1, stagger: 0.1, y: 0}), });
+ScrollTrigger.batch("div", {onEnter: batch => gsap.fromTo(batch, { duration:1 , opacity: 0, stagger: 0.1, y:100, overwrite: false }, {duration: 1, opacity: 1, stagger: 0.1, y: 0, overwrite: false}), });
+
